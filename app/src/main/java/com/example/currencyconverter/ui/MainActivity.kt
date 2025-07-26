2cb7ca38ad166bf248ea2650e99dc6faa9e75913
jj3ka <radiantgamestudios@gmail.com>
2025-07-26 18:52:16 +0300

    Start

--- app/src/main/java/com/example/currencyconverter/ui/MainActivity.kt
+++ app/src/main/java/com/example/currencyconverter/ui/MainActivity.kt
@@ -1,14 +1,39 @@
 package com.example.currencyconverter.ui
 
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.fragment.app.FragmentActivity
+import com.example.currencyconverter.data.dataSource.remote.RemoteRatesServiceImpl
 
 class MainActivity : FragmentActivity() {
+    val remoteRatesServise = RemoteRatesServiceImpl()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-
+        //enableEdgeToEdge()
+        setContent {
+            ShowCurrency()
+        }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ShowCurrency(){
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ListItem(){
+
 }
